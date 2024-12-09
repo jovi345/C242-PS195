ea5080ba64d593cf0ac193cfafa603ee991076a6
John Jovi Sidabutar <sidabutarjohnjovi@gmail.com>
2024-12-09 18:17:19 +0700

    edited recommend by last seen

--- src/handler/destinationHandler.js
+++ src/handler/destinationHandler.js
@@ -67,7 +67,7 @@ const recommendPlaceByLastseen = async (request, h) => {
   const query = 'SELECT * FROM users WHERE email = ?'
   const [row] = await dbConfig.query(query, [email])
 
-  const id = row[0].last_seen - 1
+  const id = row[0].last_seen
 
   const targetUrl = `${process.env.TARGET_URL}/${id}`
 
